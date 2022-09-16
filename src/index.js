a10eb7013608834c60a8844b1cf88286992ae819
Saeqa Sultani <saeqasultani@gmail.com>
2022-09-16 22:52:43 +0430

    Set up the project with Webpack and Jest

--- src/index.js
+++ src/index.js
@@ -1,41 +1,4 @@
 import './index.css';
-import './index.scss';
-import countData from './modules/countData';
-import dataUi from './modules/dataUi';
-import getData from './modules/getData';
-import getLike from './modules/getLike';
-import postData from './modules/postData';
 
-
-countData();
-
-const loadData = async () => {
-    const data = await getData();
-    data.forEach((item) => {
-      dataUi(item);
-    });
-  };
-  
-  loadData();
-
-
-window.like = (e) =>{
-  
-  e.classList.add('liked');
-
-  const numLike = parseInt(1,e.innerHTML);
-
-  // console.log(e.id,typeof numLike);
-  const likes =  getLike(e.id);
-  console.log(likes);
-  // e.innerHTML = likes;
-
-  postData(e.id,numLike);
- 
- 
-
- 
-  
-}
 
 
